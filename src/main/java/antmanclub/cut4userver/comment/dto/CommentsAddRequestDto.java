c6926659de6c07d241e882d7a19a620d694b291a
HongGit <soojam@konkuk.ac.kr>
2023-08-14 17:45:45 +0900

    fix: comment부분 일단 삭제

--- src/main/java/antmanclub/cut4userver/comment/dto/CommentsAddRequestDto.java
+++
@@ -1,9 +0,0 @@
-package antmanclub.cut4userver.comment.dto;
-
-import lombok.Getter;
-
-@Getter
-public class CommentsAddRequestDto {
-    private Long parentCommentId;
-    private String comment;
-}
--- src/main/java/antmanclub/cut4userver/comment/repository/CommentRepository.java
+++
@@ -1,7 +0,0 @@
-package antmanclub.cut4userver.comment.repository;
-
-import antmanclub.cut4userver.comment.domain.Comment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CommentRepository extends JpaRepository<Comment, Long> {
-}
