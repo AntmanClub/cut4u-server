a5c40ef7936c8028b1d4db4208e413e9da43afdf
seongmin0229 <leesung2925@gmail.com>
2023-08-14 17:33:10 +0900

    feat & fix : 댓글 수정기능, Comment 도메인 및 Dto 수정

--- src/main/java/antmanclub/cut4userver/comment/controller/CommentController.java
+++ src/main/java/antmanclub/cut4userver/comment/controller/CommentController.java
@@ -3,6 +3,7 @@ package antmanclub.cut4userver.comment.controller;
 import antmanclub.cut4userver.comment.domain.Comment;
 import antmanclub.cut4userver.comment.dto.CommentsAddRequestDto;
 import antmanclub.cut4userver.comment.dto.CommentsListResponseDto;
+import antmanclub.cut4userver.comment.dto.CommentsModifyRequestDto;
 import antmanclub.cut4userver.comment.service.CommentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -13,12 +14,24 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class CommentController {
     private final CommentService commentService;
+
+    @GetMapping("/{postsId}")
+    public CommentsListResponseDto viewComments(@PathVariable(value = "postsId") Long postsId) {
+        return commentService.viewComments(postsId);
+    }
+
     @PostMapping("/add")
-    public CommentsListResponseDto addComments(@RequestBody CommentsAddRequestDto commentsAddRequestDto){
+    public CommentsListResponseDto addComments(@RequestBody CommentsAddRequestDto commentsAddRequestDto) {
         return commentService.addComment(commentsAddRequestDto);
     }
-    @GetMapping("/{postsId}")
-    public CommentsListResponseDto viewComments(@PathVariable(value = "postsId") Long postsId){
-        return commentService.viewComments(postsId);
+
+    @DeleteMapping("/delete/{commentId}")
+    public CommentsListResponseDto deleteComments(@PathVariable(value = "commentId") Long commentId) {
+        return commentService.deleteComment(commentId);
+    }
+
+    @PatchMapping("/modify")
+    public CommentsListResponseDto modifyComments(@RequestBody CommentsModifyRequestDto commentsModifyRequestDto) {
+        return commentService.modifyComment(commentsModifyRequestDto);
     }
 }
--- src/main/java/antmanclub/cut4userver/comment/dto/CommentsListResponseDto.java
+++ src/main/java/antmanclub/cut4userver/comment/dto/CommentsListResponseDto.java
@@ -1,10 +1,16 @@
 package antmanclub.cut4userver.comment.dto;
 
 import lombok.Builder;
+import lombok.Getter;
 
 import java.util.List;
 
+@Getter
 public class CommentsListResponseDto {
     private List<CommentsDto> commentDtoList;
-
+    private int commentCount;
+    public CommentsListResponseDto(List<CommentsDto> commentDtoList){
+        this.commentDtoList = commentDtoList;
+        this.commentCount = commentDtoList.size();
+    }
 }
