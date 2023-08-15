abe356572692177661ae852f3a6cbb2abc461adf
HongGit <sukbum1020@gmail.com>
2023-08-15 20:04:02 +0900

    fix: posts 에러 처리 완료

--- src/main/java/antmanclub/cut4userver/global/error/ErrorCode.java
+++ src/main/java/antmanclub/cut4userver/global/error/ErrorCode.java
@@ -10,19 +10,19 @@ public enum ErrorCode
     // User
     USER_NOT_FOUND(400, "U001", "유저가 존재하지 않습니다."),
     NOT_CORRECT_PASSWORD(400, "U002", "비밀번호가 틀렸습니다."),
-    ALREADY_EXIST_NAME(400, "M005", "이미 있는 이름입니다."),
-    ALREADY_EXIST_EMAIL(400, "M005", "이미 있는 이메일입니다."),
-    NOT_EQUAL_PASSWORD(400, "M005", "비밀번호가 일치하지 않습니다."),
-    CAN_NOT_FOLLOW_MYSELF(400, "M005", "자기 자신은 팔로우할 수 없습니다."),
-    ALREADY_FOLLOW_USER(400, "M005", "이미 팔로우한 유저입니다."),
-    CAN_NOT_UNFOLLOW_MYSELF(400, "M005", "자기 자신은 언팔로우할 수 없습니다."),
-    NOT_FOLLOW_USER(400, "M005", "팔로우하지 않은 유저입니다."),
+    ALREADY_EXIST_NAME(400, "U003", "이미 있는 이름입니다."),
+    ALREADY_EXIST_EMAIL(400, "U004", "이미 있는 이메일입니다."),
+    NOT_EQUAL_PASSWORD(400, "U005", "비밀번호가 일치하지 않습니다."),
+    CAN_NOT_FOLLOW_MYSELF(400, "U006", "자기 자신은 팔로우할 수 없습니다."),
+    ALREADY_FOLLOW_USER(400, "U007", "이미 팔로우한 유저입니다."),
+    CAN_NOT_UNFOLLOW_MYSELF(400, "U008", "자기 자신은 언팔로우할 수 없습니다."),
+    NOT_FOLLOW_USER(400, "U009", "팔로우하지 않은 유저입니다."),
 
     // Posts
-    DAILYPLAN_ALREADY_EXIST(400, "D001", "이미 데일리 플랜을 작성하였습니다. PUT으로 update 해주세요"),
-    DAILYPLAN_NOT_FOUND(400, "D002", "존재 하지 않는 데일리 플랜입니다"),
-    DAILYPLAN_MONTHLIST_NOT_FOUND(400, "D003", "해당 월에 작성된 데일리 플랜이 없습니다"),
-    DAILYPLAN_NOT_HAVE_TODOLIST(400, "D004", "해당 데일리 플랜에 작성된 투두리스트가 없습니다"),
+    POSTS_PIC_NOT_FOUR(400, "P001", "사진의 개수가 4장이 아닙니다."),
+    POSTS_NOT_FOUND(400, "P002", "게시물을 찾을 수 없습니다."),
+    POSTS_CAN_NOT_DELETE(400, "P003", "게시물을 삭제할 수 없습니다."),
+
 
     // Comment
     TODOLIST_NOT_FOUND(400, "T001", "존재 하지 않는 투두리스트입니다"),
