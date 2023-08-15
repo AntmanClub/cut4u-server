9924a0fbf04c053712f6f6b935364a8ea1440fda
HongGit <sukbum1020@gmail.com>
2023-08-15 21:34:29 +0900

    fix: comment 성공 응답 처리 완료

--- src/main/java/antmanclub/cut4userver/comment/controller/CommentController.java
+++ src/main/java/antmanclub/cut4userver/comment/controller/CommentController.java
@@ -5,7 +5,10 @@ import antmanclub.cut4userver.comment.dto.CommentsAddRequestDto;
 import antmanclub.cut4userver.comment.dto.CommentsListResponseDto;
 import antmanclub.cut4userver.comment.dto.CommentsModifyRequestDto;
 import antmanclub.cut4userver.comment.service.CommentService;
+import antmanclub.cut4userver.global.result.ResultCode;
+import antmanclub.cut4userver.global.result.ResultResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,22 +19,26 @@ public class CommentController {
     private final CommentService commentService;
 
     @GetMapping("/{postsId}")
-    public CommentsListResponseDto viewComments(@PathVariable(value = "postsId") Long postsId) {
-        return commentService.viewComments(postsId);
+    public ResponseEntity<ResultResponse> viewComments(@PathVariable(value = "postsId") Long postsId) {
+        CommentsListResponseDto data = commentService.viewComments(postsId);
+        return ResponseEntity.ok(ResultResponse.of(ResultCode.COMMENT_GET_SUCCESS, data));
     }
 
     @PostMapping("/add")
-    public CommentsListResponseDto addComments(@RequestBody CommentsAddRequestDto commentsAddRequestDto) {
-        return commentService.addComment(commentsAddRequestDto);
+    public ResponseEntity<ResultResponse> addComments(@RequestBody CommentsAddRequestDto commentsAddRequestDto) {
+        CommentsListResponseDto data = commentService.addComment(commentsAddRequestDto);
+        return ResponseEntity.ok(ResultResponse.of(ResultCode.COMMENT_SAVE_SUCCESS, data));
     }
 
     @DeleteMapping("/delete/{commentId}")
-    public CommentsListResponseDto deleteComments(@PathVariable(value = "commentId") Long commentId) {
-        return commentService.deleteComment(commentId);
+    public ResponseEntity<ResultResponse> deleteComments(@PathVariable(value = "commentId") Long commentId) {
+        CommentsListResponseDto data = commentService.deleteComment(commentId);
+        return ResponseEntity.ok(ResultResponse.of(ResultCode.COMMENT_DELETE_SUCCESS, data));
     }
 
     @PatchMapping("/modify")
-    public CommentsListResponseDto modifyComments(@RequestBody CommentsModifyRequestDto commentsModifyRequestDto) {
-        return commentService.modifyComment(commentsModifyRequestDto);
+    public ResponseEntity<ResultResponse> modifyComments(@RequestBody CommentsModifyRequestDto commentsModifyRequestDto) {
+        CommentsListResponseDto data = commentService.modifyComment(commentsModifyRequestDto);
+        return ResponseEntity.ok(ResultResponse.of(ResultCode.COMMENT_MODIFY_SUCCESS, data));
     }
 }
