214f140bcc9ca4fc0fa1ff05d3dbef2e3ea401a8
HongGit <soojam@konkuk.ac.kr>
2023-08-18 17:21:38 +0900

    fix: error code 400으로 수정

---
+++ src/main/java/antmanclub/cut4userver/posts/controller/HashtagController.java
@@ -0,0 +1,12 @@
+package antmanclub.cut4userver.posts.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/hashtag")
+public class HashtagController {
+
+}
